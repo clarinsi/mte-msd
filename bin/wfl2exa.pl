51026dc27623f55ecb4cae4608cbd3d3cf6ac305
Tomaž Erjavec <tomaz.erjavec@ijs.si>
2018-11-09 20:33:26 +0100

    Better generation of examples

--- bin/wfl2exa.pl
+++ bin/wfl2exa.pl
@@ -1,16 +1,18 @@
 #!/usr/bin/perl -w
+# From a MULTEXT-type lexicon takes $max examples of each MSD
 $max = shift;
 use utf8;
 binmode(STDIN,'utf8');
 binmode(STDOUT,'utf8');
 binmode(STDERR,'utf8');
-my $freq;
+my $has_freq;
 while (<>) {
     chomp;
     ($word, $lemma, $msd, $freq) = split /\t/;
-    $freq = 0 unless $freq and $freq =~ /^\d+$/;
+    $has_freq = 1 if $freq and $freq =~ /^\d+$/;
+    $freq = 0 unless $freq;
     $max{$msd}++;
-    if ($max{$msd} <= $freq) {
+    if ($max{$msd} <= $max) {
 	push @{$lex{$msd}}, "$word\t$lemma";
     }
     $msd_type{$msd}++;
@@ -20,7 +22,7 @@ foreach $msd (sort keys %lex) {
     foreach $lex (@{$lex{$msd}}) {
 	print "$lex\t$msd\t$msd_type{$msd}";
 	print "\t$msd_token{$msd}"
-	    if exists $msd_token{$msd} and $msd_token{$msd};
+	    if $has_freq;
 	print "\n"
     }
 }
